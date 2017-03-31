c4fb8d6cc61221ba420a3ab43ec9331c438d564e
Samuel A. Falvo II <kc5tja@arrl.net>
2017-03-30 23:01:01 -0700

    Implementation of the queue.

--- queue/queue_tb.v
+++ queue/queue_tb.v
@@ -1,5 +1,6 @@
 `timescale 1ns / 1ps
 
+
 `include "asserts.vh"
 
 
@@ -8,8 +9,7 @@ module queue_tb();
 
 	reg		clk_i, reset_i;
 	reg	[7:0]	dat_i;
-	reg		push_i;
-	reg		pop_i;
+	reg		push_i, pop_i, oe_i;
 
 	wire	[7:0]	dat_o;
 	wire		full_o, empty_o;
@@ -30,13 +30,18 @@ module queue_tb();
 
 		.dat_o(dat_o),
 		.pop_i(pop_i),
+		.oe_i(oe_i),
 		.full_o(full_o),
 		.empty_o(empty_o),
 
 		.rp_to(rp_to),
 		.wp_to(wp_to),
-		.we_to(we_to)
+		.room_to(room_to)
 	);
+
+	always begin
+		#20 clk_i <= ~clk_i;
+	end
 
 	task story;
 	input [11:0] expected;
@@ -55,9 +60,7 @@ module queue_tb();
 
 	task tick;
 	begin
-		#19
-		clk_i <= 0; #20
-		clk_i <= 1; #1;
+		wait(clk_i); wait(~clk_i);
 	end
 	endtask
 
@@ -65,10 +68,10 @@ module queue_tb();
 		$dumpfile("wtf.vcd");
 		$dumpvars;
 
-		{clk_i, reset_i, dat_i, push_i} <= 0;
+		{clk_i, reset_i, dat_i, push_i, pop_i, oe_i} <= 0;
 		story(0);
 
-		tick;
+		wait(~clk_i);
 
 		reset_i <= 1;
 
@@ -77,6 +80,9 @@ module queue_tb();
 		assert_rp(0);
 		assert_wp(0);
 		assert_room(8);
+		assert_dat(0);
+		assert_empty(1);
+		assert_full(0);
 
 		story(2);
 
@@ -86,87 +92,210 @@ module queue_tb();
 		dat_i <= 8'hFF;
 
 		tick;
+
 		assert_room(7);
 		assert_wp(1);
 		assert_rp(0);
+		assert_dat(0);
+		assert_empty(0);
+		assert_full(0);
 		tick;
 		assert_room(6);
 		assert_wp(2);
 		assert_rp(0);
+		assert_dat(0);
+		assert_empty(0);
+		assert_full(0);
 		tick;
 		assert_room(5);
 		assert_wp(3);
 		assert_rp(0);
+		assert_dat(0);
+		assert_empty(0);
+		assert_full(0);
 		tick;
 		assert_room(4);
 		assert_wp(4);
 		assert_rp(0);
+		assert_dat(0);
+		assert_empty(0);
+		assert_full(0);
 		tick;
 		assert_room(3);
 		assert_wp(5);
 		assert_rp(0);
+		assert_dat(0);
+		assert_empty(0);
+		assert_full(0);
 		tick;
 		assert_room(2);
 		assert_wp(6);
 		assert_rp(0);
+		assert_dat(0);
+		assert_empty(0);
+		assert_full(0);
 		tick;
 		assert_room(1);
 		assert_wp(7);
 		assert_rp(0);
+		assert_dat(0);
+		assert_empty(0);
+		assert_full(0);
 		tick;
 		assert_room(0);
 		assert_wp(0);
 		assert_rp(0);
+		assert_dat(0);
+		assert_empty(0);
+		assert_full(1);
 		tick;
 		assert_room(0);
 		assert_wp(0);
 		assert_rp(0);
+		assert_dat(0);
+		assert_empty(0);
+		assert_full(1);
 
 		// Popping the queue should advance the read pointer.
 
 		story(3);
 
 		push_i <= 0;
-		pop_i <= 1;		// Also drives data outputs
+		pop_i <= 1;		// Does NOT drive data outputs.
+		oe_i <= 1;		// THIS does.
 
 		tick;
 		assert_room(1);
 		assert_wp(0);
 		assert_rp(1);
+		assert_dat(255);
+		assert_empty(0);
+		assert_full(0);
 		tick;
 		assert_room(2);
 		assert_wp(0);
 		assert_rp(2);
+		assert_dat(255);
+		assert_empty(0);
+		assert_full(0);
 		tick;
 		assert_room(3);
 		assert_wp(0);
 		assert_rp(3);
+		assert_dat(255);
+		assert_empty(0);
+		assert_full(0);
 		tick;
 		assert_room(4);
 		assert_wp(0);
 		assert_rp(4);
+		assert_dat(255);
+		assert_empty(0);
+		assert_full(0);
 		tick;
 		assert_room(5);
 		assert_wp(0);
 		assert_rp(5);
+		assert_dat(255);
+		assert_empty(0);
+		assert_full(0);
 		tick;
 		assert_room(6);
 		assert_wp(0);
 		assert_rp(6);
+		assert_dat(255);
+		assert_empty(0);
+		assert_full(0);
 		tick;
 		assert_room(7);
 		assert_wp(0);
 		assert_rp(7);
+		assert_dat(255);
+		assert_empty(0);
+		assert_full(0);
 		tick;
 		assert_room(8);
 		assert_wp(0);
 		assert_rp(0);
+		assert_dat(255);
+		assert_empty(1);
+		assert_full(0);
 		tick;
 		assert_room(8);
 		assert_wp(0);
 		assert_rp(0);
+		assert_dat(255);
+		assert_empty(1);
+		assert_full(0);
 
 		// The FIFO must support concurrent reads and writes.
+		// Testing strategy is simple:
+		// First, we write all $AA bytes to the queue's storage.
+		// From our previous tests, we know the data bus must be $FF.
+
+		story(4);
+
+		dat_i <= 8'hAA;
+		push_i <= 1;
+		pop_i <= 1;		// Does NOT drive data outputs.
+		oe_i <= 1;		// THIS does.
+
+		tick;
+		assert_room(8);
+		assert_wp(1);
+		assert_rp(1);
+		assert_dat(255);
+		assert_empty(1);
+		assert_full(0);
+		tick;
+		assert_room(8);
+		assert_wp(2);
+		assert_rp(2);
+		assert_dat(255);
+		assert_empty(1);
+		assert_full(0);
+		tick;
+		assert_room(8);
+		assert_wp(3);
+		assert_rp(3);
+		assert_dat(255);
+		assert_empty(1);
+		assert_full(0);
+		tick;
+		assert_room(8);
+		assert_wp(4);
+		assert_rp(4);
+		assert_dat(255);
+		assert_empty(1);
+		assert_full(0);
+		tick;
+		assert_room(8);
+		assert_wp(5);
+		assert_rp(5);
+		assert_dat(255);
+		assert_empty(1);
+		assert_full(0);
+		tick;
+		assert_room(8);
+		assert_wp(6);
+		assert_rp(6);
+		assert_dat(255);
+		assert_empty(1);
+		assert_full(0);
+		tick;
+		assert_room(8);
+		assert_wp(7);
+		assert_rp(7);
+		assert_dat(255);
+		assert_empty(1);
+		assert_full(0);
+		tick;
+		assert_room(8);
+		assert_wp(0);
+		assert_rp(0);
+		assert_dat(8'hAA);
+		assert_empty(1);
+		assert_full(0);
 
 		// (after writing the 8th item to the queue,
 		// we should wrap around, and see the first
@@ -176,4 +305,3 @@ module queue_tb();
 		$stop;
 	end
 endmodule
-
